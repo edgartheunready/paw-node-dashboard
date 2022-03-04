8b367eed40b3360dfb52850687f6f396d970ad9a
Jared Menard <menard.jared@gmail.com>
2022-03-03 21:37:38 -0500

    added tailwind CDN link

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,13 +2,17 @@ class TransactionsController < ApplicationController
   before_action :set_tribe, only: %i[ index ]
   # GET /tribes or /tribes.json
   def index
-    @transactions = @tribe.transactions
+    if @tribe.present?
+      @transactions = @tribe.transactions
+    else
+      @transactions = Transaction.all
+    end
   end
 
   private
 
   def set_tribe
-    @tribe = Tribe.find(params[:tribe_id])
+    @tribe = Tribe.find(params[:tribe_id]) if params[:tribe_id].present?
   end
 
 end
